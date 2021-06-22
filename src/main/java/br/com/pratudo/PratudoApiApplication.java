c3df178ddb9b5abfdf1107d4d48234cb98ae5fbf
RodrigoLimaM <digo100200@gmail.com>
2021-06-21 22:17:49 -0300

    inicializando projeto

---
+++ src/main/java/br/com/pratudo/PratudoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pratudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PratudoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PratudoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pratudo/PratudoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pratudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PratudoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

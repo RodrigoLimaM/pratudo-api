84d4d9101cab805e09021726c1be56cf6618786a
RodrigoLimaM <digo100200@gmail.com>
2021-06-24 23:03:18 -0300

    adicionando credenciais

---
+++ src/main/java/br/com/pratudo/config/security/FeignConfig.java
@@ -0,0 +1,22 @@
+package br.com.pratudo.config.security;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor(username, password);
+    }
+}
--- src/main/java/br/com/pratudo/user/client/UserClient.java
+++ src/main/java/br/com/pratudo/user/client/UserClient.java
@@ -1,6 +1,7 @@
 package br.com.pratudo.user.client;
 
 import br.com.pratudo.commons.search.SearchParams;
+import br.com.pratudo.config.security.FeignConfig;
 import br.com.pratudo.user.model.ElasticsearchSingleUser;
 import br.com.pratudo.user.model.ElasticsearchUser;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "UserClient", url = "${elasticsearch.url}")
+@FeignClient(name = "UserClient", url = "${elasticsearch.url}", configuration = FeignConfig.class, decode404 = true)
 public interface UserClient {
 
     String INDEX = "/pratudo/users";

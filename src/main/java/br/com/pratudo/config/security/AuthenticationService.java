bd1d87036e43364157d9127ab5b4c7ec56485210
RodrigoLimaM <digo100200@gmail.com>
2021-06-26 11:31:28 -0300

    alterando lógica de autenticação

--- src/main/java/br/com/pratudo/config/security/AuthenticationService.java
+++ src/main/java/br/com/pratudo/config/security/AuthenticationService.java
@@ -2,6 +2,7 @@ package br.com.pratudo.config.security;
 
 import br.com.pratudo.commons.search.SearchParamsFactory;
 import br.com.pratudo.user.client.UserClient;
+import br.com.pratudo.user.model.ElasticsearchUser;
 import br.com.pratudo.user.model.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,8 +21,16 @@ public class AuthenticationService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userMapper.convertElasticsearchUserToUser(
-                userClient.getUserByEmail(SearchParamsFactory.buildGetUserByEmailParams(email))
-        );
+        ElasticsearchUser elasticsearchUser = userClient.getUserByEmail(SearchParamsFactory.buildGetUserByEmailParams(email));
+
+        if(!isUserExistent(elasticsearchUser)) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return userMapper.convertElasticsearchUserToUser(elasticsearchUser);
+    }
+
+    private boolean isUserExistent(ElasticsearchUser elasticsearchUser) {
+        return !elasticsearchUser.getHits().getHits().isEmpty();
     }
 }

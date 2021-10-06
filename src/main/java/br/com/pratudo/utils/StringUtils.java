3c12743d959f6978ace55c53a686b73edfa375c9
RodrigoLimaM <digo100200@gmail.com>
2021-10-05 21:51:52 -0300

    removendo metodo nao utilizado

--- src/main/java/br/com/pratudo/utils/StringUtils.java
+++ src/main/java/br/com/pratudo/utils/StringUtils.java
@@ -1,20 +1,13 @@
 package br.com.pratudo.utils;
 
-import br.com.pratudo.recipe.model.enums.Category;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class StringUtils {
 
     public String convertListToStringSeparatedWithCommas(List<String> ingredients) {
         return ingredients.toString().replace("[", "").replace("]", "");
-    }
-
-    public String convertListToStringWithQuotationMarks(List<Category> categories) {
-        return categories.stream().map(category -> "\"" +category +"\"")
-                .collect(Collectors.toList()).toString();
     }
 }
